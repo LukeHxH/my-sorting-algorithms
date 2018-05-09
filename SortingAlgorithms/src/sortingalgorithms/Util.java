0360d6fcd388b130452e964f4dea3ff771cb3980
LukeHxH <lucasmed812@gmail.com>
2018-05-09 15:00:01 -0300

    classe util

---
+++ SortingAlgorithms/src/sortingalgorithms/Util.java
@@ -0,0 +1,14 @@
+package sortingalgorithms;
+
+public class Util {
+	
+	public static void swap(Object[] array, int i, int j) {
+		if (array == null)
+			throw new IllegalArgumentException("Array nulo!");
+		
+		Object temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+
+}
