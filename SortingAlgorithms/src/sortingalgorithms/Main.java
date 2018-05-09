7a6ac763e02590e4c553632bd277e13a772e4886
LukeHxH <lucasmed812@gmail.com>
2018-05-09 18:10:07 -0300

    quicksort + main methods

--- SortingAlgorithms/src/sortingalgorithms/Main.java
+++ SortingAlgorithms/src/sortingalgorithms/Main.java
@@ -1,10 +1,43 @@
 package sortingalgorithms;
 
+import java.util.Arrays;
+
 public class Main {
+	
+	private static Integer[] arrayEvenSize;
+	private static Integer[] arrayOddSize;
+	private static Integer[] emptyArray;
+	private static Integer[] arrayWithRepeatedElements;
+	private static Integer[] arraySingleValue;
+	
+	private static AbstractSorting<Integer> implementation;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		arrayEvenSize = new Integer[] {10, 8, 12, 5, 2, 9, 3, 7, -2, 4};
+		arrayOddSize = new Integer[] {10, 8, 12, 5, 2, 9, 3, 7, 1, -2};
+		arrayWithRepeatedElements = new Integer[] {5, 2, 6, 8, 7, 5, 6, 1, 3, 5, 4, -2};
+		emptyArray = new Integer[] {};
+		
+		implementation = new BubbleSort<>();
+		/* Change this to test a sorting algorithm.
+		 * 
+		 * For example: implementation = new QuickSort<>();
+		 * */
+		
+		printSortedArray(arrayEvenSize, 2, 6);
+		// Change this if you want to test another arrray.
+	}
+	
+	// Sorts an array from first index to last
+	public static void printSortedArray(Integer[] array) {
+		printSortedArray(array, 0, array.length - 1);
+	}
+	
+	// Sorts an array from an index to other.
+	public static void printSortedArray(Integer[] array, int left, int right) {
+		System.out.println("BEFORE SORTING: " + Arrays.toString(array));
+		implementation.sort(array, left, right);
+		System.out.println("AFTER SORTING: " + Arrays.toString(array));
 	}
 
 }
--- SortingAlgorithms/src/sortingalgorithms/QuickSort.java
+++ SortingAlgorithms/src/sortingalgorithms/QuickSort.java
@@ -1,10 +1,35 @@
 package sortingalgorithms;
 
-public class QuickSort<T extends Comparable<T>> extends AbstractSorting<T> {
+import java.util.concurrent.ThreadLocalRandom;
 
+public class QuickSort<T extends Comparable<T>> extends AbstractSorting<T> {
+	
+	// Randomized pivot included!
+	
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
+		if (leftIndex < rightIndex) {
+			int indexPivot = partition(array, leftIndex, rightIndex);
+			sort(array, leftIndex, indexPivot - 1);
+			sort(array, indexPivot + 1, rightIndex);
+		}
+	}
+
+	private int partition(T[] array, int leftIndex, int rightIndex) {
+		int randomIndex = ThreadLocalRandom.current().nextInt(leftIndex, rightIndex + 1);
+		int indexPivot = leftIndex;
 		
+		Util.swap(array, randomIndex, indexPivot);
+		
+		T pivot = array[leftIndex];
+		
+		for (int i = leftIndex + 1; i <= rightIndex; i++) {
+			if (array[i].compareTo(pivot) <= 0)
+				Util.swap(array, i, ++indexPivot);
+		}
+		
+		Util.swap(array, leftIndex, indexPivot);
+		return indexPivot;
 	}
 
 }
